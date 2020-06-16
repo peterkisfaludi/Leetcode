6499134e98101133459968ea46a4a258170c8d88
Peter Kisfaludi <pkissfa@gmail.com>
2020-06-16 11:11:34 +0200

    Create 154_find-minimum-in-rotated-sorted-array-ii.cpp

---
+++ 154_find-minimum-in-rotated-sorted-array-ii.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int findMin(vector<int>& A) {
+        int N=A.size();
+        if(N==0){return 0;}
+        if(N==1){return A[0];}
+        if(N==2){return min(A[0],A[1]);}
+        if(N==3){return min(A[0],min(A[1],A[2]));}
+        
+        int B=0;
+        int E=N-1;
+        int M;
+        
+        while(1){
+            M=B+(E-B)/2;
+            
+            if(E-B <= 2){
+                return min(A[B],min(A[M],A[E]));
+            }
+            
+            if(A[B] == A[M] && A[M] == A[E]){
+                return *min_element(A.begin(), A.end());
+            }
+            if(A[B] <= A[M] && A[M] <= A[E]){return A[B];}
+            
+            if(A[B]<=A[M]){B=M;continue;}
+            if(A[E]>=A[M]){E=M;continue;}
+        }
+    }
+};
