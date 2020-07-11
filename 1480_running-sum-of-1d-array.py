4c6b9f3ab55b7285e8cb75d234fbc6ce60274def
Peter Kisfaludi <pkissfa@gmail.com>
2020-07-11 05:51:49 +0200

    Update 1480_running-sum-of-1d-array.py

--- 1480_running-sum-of-1d-array.py
+++ 1480_running-sum-of-1d-array.py
@@ -1,10 +1,8 @@
 from typing import List
 class Solution:
     def runningSum(self, nums: List[int]) -> List[int]:
-        N=len(nums)
-        if N==0: return []
-        ret=[0]*N
-        ret[0]=nums[0]
-        for i in range(1, N):
-            ret[i]=ret[i-1]+nums[i]
-        return ret
+        n=len(nums)
+        for i in range(1, n):
+            nums[i]+=nums[i-1]
+        return nums
+        
